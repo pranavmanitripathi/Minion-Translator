0c22e10f5e9ddc0e300a49b61796452b9818bd1e
PRANAV MANI TRIPATHI <pranavtripathikishan26@gmail.com>
2020-11-30 14:52:44 +0530

    added minion translator api

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-// var user= prompt("give the user name ");
-
-// alert("name is "+user);
 
 var btntranslate = document.querySelector("#btn-translator");
 var txtinput = document.querySelector("#txt-input");
 var otput = document.querySelector("#output");
 
+var serverurl = "https://api.funtranslations.com/translate/minion.json";
 
-btntranslate.addEventListener("click",function btntrans(){
-    otput.innerText = txtinput.value;
+function generateurl(text){
+    return serverurl +"?"+"text="+text;
+}
+function errorHandler(error){
+    alert("something wrong with server! try again after some time")
+}
 
-   
-})
+function btntrans(){
+    var inputtext =  txtinput.value;
+
+    fetch(generateurl(inputtext)).
+    then(response => response.json()).
+    then(json => otput.innerText=json.contents.translated)
+    .catch(errorHandler)
+};
+
+
+btntranslate.addEventListener("click", btntrans)
+    
