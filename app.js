4a778a39fd0d56ec5419432208e586888df03369
PRANAV MANI TRIPATHI <pranavtripathikishan26@gmail.com>
2020-11-30 10:51:57 +0530

    added input taking feature

---
+++ app.js
@@ -0,0 +1,10 @@
+// var user= prompt("give the user name ");
+
+// alert("name is "+user);
+
+var btntranslate = document.querySelector("#btn-translator");
+var txtinput = document.querySelector("#txt-input");
+
+btntranslate.addEventListener("click",function btntrans(){
+    console.log(txtinput.value);
+})
